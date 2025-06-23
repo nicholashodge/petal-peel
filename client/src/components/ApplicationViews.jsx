6f3a69ffb8ae55cb26da274b8ebe8d0e22b6ad4c
nicholashodge <nicholas.m.hodge@gmail.com>
2025-06-23 07:53:10 -0500

    updated ApplicationViews with all MVP pages

--- client/src/components/ApplicationViews.jsx
+++ client/src/components/ApplicationViews.jsx
@@ -3,6 +3,11 @@ import { AuthorizedRoute } from "./auth/AuthorizedRoute";
 import { Mocktails } from "./views/Mocktails";
 import Register from "./auth/Register";
 import Login from "./auth/Login";
+import MocktailDetails from "./views/MocktailDetails";
+import EditMocktail from "./views/EditMocktail";
+import MyRecipes from "./views/MyRecipes";
+import AccountDetails from "./views/AccountDetails";
+import EditAccount from "./views/EditAccount";
 
 export default function ApplicationViews({ loggedInUser, setLoggedInUser }) {
     return (
@@ -12,18 +17,18 @@ export default function ApplicationViews({ loggedInUser, setLoggedInUser }) {
           index
           element={
             <AuthorizedRoute loggedInUser={loggedInUser}>
-                <Mocktails />
+                <Mocktails loggedInUser={loggedInUser} />
             </AuthorizedRoute>
           }
         />
-        <Route
-        path="login"
-        element={<Login setLoggedInUser={setLoggedInUser} />}
-        />
-        <Route
-        path="register"
-        element={<Register setLoggedInUser={setLoggedInUser} />}
-        />
+        <Route path="login" element={<Login setLoggedInUser={setLoggedInUser} />} />
+        <Route path="register" element={<Register setLoggedInUser={setLoggedInUser} />} />
+        <Route path="account" element={<AccountDetails loggedInUser={loggedInUser} />} />
+        <Route path="account/edit" element={<EditAccount loggedInUser={loggedInUser} />} />
+        <Route path="mocktails" element={<Mocktails />} />
+        <Route path="mocktails/:id" element={<MocktailDetails />} />
+        <Route path="mocktails/edit/:id" element={<EditMocktail />} />
+        <Route path="mocktail/mine" element={<MyRecipes />} />
         </Route>
         <Route path="*" element={<p>Whoops, nothing here...</p>} />
     </Routes>
